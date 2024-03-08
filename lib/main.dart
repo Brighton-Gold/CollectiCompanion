4e3e6d70677f285aaaedeca782cff468fc15b935
Brighton-Gold <113638673+Brighton-Gold@users.noreply.github.com>
2024-03-08 16:24:29 -0700

    Created the login page ans synced with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey:
+          "AIzaSyBCwCnZfJsBlBbqee04V66_AoboLrEnXKA", // Replace with actual keys
+      authDomain: "collecticompanion.firebaseapp.com",
+      projectId: "collecticompanion",
+      storageBucket: "collecticompanion.appspot.com",
+      messagingSenderId: "987253529390",
+      appId: "1:987253529390:web:63ef90dd35c929b956f8bf",
+    ),
+  );
   runApp(const MyApp());
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
+      body: const Center(
+        child: Text("You are logged in!"),
+      ),
+    );
+  }
+}
+
+class AuthenticationService {
+  final FirebaseAuth _firebaseAuth;
+
+  AuthenticationService(this._firebaseAuth);
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  Future<void> signIn({required String email, required String password}) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -39,6 +82,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+/*
 class _MyHomePageState extends State<MyHomePage> {
   final PageController pageController = PageController(initialPage: 0);
   late int _selectedIndex = 0;
@@ -72,14 +116,14 @@ class _MyHomePageState extends State<MyHomePage> {
         shape: const CircularNotchedRectangle(),
         notchMargin: 5.0,
         clipBehavior: Clip.antiAlias,
-        padding: EdgeInsets.fromLTRB(3, 0, 3, 1),
+        padding: const EdgeInsets.fromLTRB(3, 0, 3, 1),
         child: SizedBox(
           height: kBottomNavigationBarHeight,
           child: BottomNavigationBar(
             type: BottomNavigationBarType.fixed,
-            backgroundColor: Color.fromARGB(255, 211, 136, 43),
+            backgroundColor: const Color.fromARGB(255, 211, 136, 43),
             currentIndex: _selectedIndex,
-            selectedItemColor: Color.fromARGB(255, 254, 250, 8),
+            selectedItemColor: const Color.fromARGB(255, 254, 250, 8),
             unselectedItemColor: Colors.black,
             onTap: (index) {
               setState(() {
@@ -111,6 +155,15 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: LoginScreen(),
+    );
+  }
+}
 
 // Screens for the different bottom navigation items
 class Home extends StatelessWidget {
@@ -122,7 +175,7 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Home'),
         centerTitle: true,
-        backgroundColor: Color.fromARGB(255, 211, 136, 43),
+        backgroundColor: const Color.fromARGB(255, 211, 136, 43),
       ),
       body: const Center(
         child: Image(image: AssetImage('assets/collectiLogo.png')),
@@ -130,7 +183,6 @@ class Home extends StatelessWidget {
     );
   }
 }
-
 
 class Search extends StatelessWidget {
   const Search({Key? key}) : super(key: key);
@@ -156,5 +208,91 @@ class Add extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Text('Create new Database');
+  }
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<void> _signIn() async {
+    try {
+      UserCredential userCredential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+      print("User ID: ${userCredential.user?.uid}");
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_) => const HomePage()));
+    } on FirebaseAuthException catch (e) {
+      // You can show an alert dialog or a snackbar here
+      print("Error: ${e.message}");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              "Welcome to CollectiCompanion",
+              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: _signIn,
+              child: const Text("Login"),
+            ),
+            TextButton(
+              onPressed: () {
+                // TODO: Navigate to Forgot Password Screen
+              },
+              child: const Text("Forgot Password?"),
+            ),
+            TextButton(
+              onPressed: () {
+                // TODO: Navigate to Sign Up Screen
+              },
+              child: const Text("Don't have an account? Sign up"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
